8fec511b5fe5d145a33a5bc0d75effa68ee049f1
seachelle <143592080+seachellea@users.noreply.github.com>
2023-11-05 20:04:26 +0800

    Created game function
    The game() guntion loops playRound() function for 5 times for user to play for 5 rounds

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 const getComputerChoice = () => {
     let random = Math.floor(Math.random() * 3);
 
@@ -11,40 +14,55 @@ const getComputerChoice = () => {
 }
 
 const computerChoice = getComputerChoice();
-console.log(computerChoice);
+// console.log(`Computer chose: ${computerChoice}`);
 
-// ======== PROMPT PLAYER CHOICE ============ //
+// ======== GAME 5 ROUNDS ============ //
 
-const getPlayerChoice = () => {
-    let pChoice = prompt("What is your pick? (Rock, Paper, Scissors): ");
-    pChoice = pChoice.charAt(0).toUpperCase() + pChoice.slice(1).toLowerCase();
+const game = () => {
 
-    if (pChoice === "Rock" || pChoice === "Paper" || pChoice === "Scissors") {
-        return pChoice;
-    } else {
-        return "Invalid choice";
-    }
-}
 
-const playerChoice = getPlayerChoice();
-console.log(playerChoice);
+    for (round = 1; round <= 5; round ++) {
 
-// ======== PROMPT PLAYER CHOICE ============ //
+        // ======== PROMPT PLAYER CHOICE ============ //
+        const getPlayerChoice = () => {
+            let pChoice = prompt("What is your pick? (Rock, Paper, Scissors): ");
+            pChoice = pChoice.charAt(0).toUpperCase() + pChoice.slice(1).toLowerCase();
 
-const playRound = (playerChoice, computerChoice) => {
+            if (pChoice === "Rock" || pChoice === "Paper" || pChoice === "Scissors") {
+                return pChoice;
+            } else {
+                return "Invalid choice";
+            }
+        }
+
+        const playerChoice = getPlayerChoice();
+        // console.log(`You chose: ${playerChoice}`); 
+
+        console.log(`Round #${round}`);
+
+        const playRound = (playerChoice, computerChoice) => {
     
-    if (playerChoice === computerChoice) {
-        return "It's a tie!";
-    }
-    else if (playerChoice === "Scissors" && computerChoice === "Paper" ||
-    playerChoice === "Paper" && computerChoice === "Rock" ||
-    playerChoice === "Rock" && computerChoice === "Scissors") {
-        return `Player wins this round! ${playerChoice} beats ${computerChoice}.`;
-    }
-    else {
-        return `Computer wins this round. ${computerChoice} beats ${playerChoice}.`;
+            if (playerChoice === computerChoice) {
+                return "It's a tie!";
+            }
+            else if (playerChoice === "Scissors" && computerChoice === "Paper" ||
+            playerChoice === "Paper" && computerChoice === "Rock" ||
+            playerChoice === "Rock" && computerChoice === "Scissors") {
+                playerScore++;
+                return `You win! ${playerChoice} beats ${computerChoice}.`;
+
+            }
+            else {
+                computerScore++;
+                return `Computer wins. ${computerChoice} beats ${playerChoice}.`;
+            }
+        }
+        
+        console.log(`You chose: ${playerChoice}; Computer chose: ${computerChoice}`);
+        const roundResult = playRound(playerChoice, computerChoice);
+        console.log(roundResult);
+        console.log(`Your score: ${playerScore} | Computer score: ${computerScore}`); 
     }
 }
 
-const roundResult = playRound(playerChoice, computerChoice);
-console.log(roundResult);
+game();
